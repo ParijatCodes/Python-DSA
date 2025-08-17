41adf9c1832fffc0f4689d1f846e99dec7bab6e9
Parijat Kundu <parijatkundu123@gmail.com>
2025-08-18 00:34:16 +0530

    Add student info script and requirements file
    Added q64.py, a script that collects student name, marks, and mobile number, then prints a formatted summary. Also added requirements.txt with necessary Python package dependencies.

---
+++ q64.py
@@ -0,0 +1,6 @@
+#Using format function
+name= input("Write name:")
+marks=int(input("Type your marks:"))
+mobile=int(input("Type your Mobile NO:"))
+S= "The name of the student is {}, he got {} out of 100 and his mobile No is {}".format(name,marks,mobile)
+print(S)
